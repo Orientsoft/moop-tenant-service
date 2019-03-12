bf04a65c578fef80427ca663194cc5029338be33
nickery <xuyilin@orientsoft.cn>
2019-03-12 15:26:49 +0800

    修改logging等级及格式

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from app import app
 import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(format=app.config['LOG_FORMAT'], level=logging.DEBUG)
+    logging.basicConfig(format=app.config['LOG_FORMAT'], level=logging.ERROR,
+                        datefmt='%Y-%m-%d %H:%M:%S')
     app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'], threaded=True)
