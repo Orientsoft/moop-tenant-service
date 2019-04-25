01568c6a3795ee7693443e5a15bdd47089bf8b4d
nickery <xuyilin@orientsoft.cn>
2019-04-25 11:50:02 +0800

    更改日志级别

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from app import app
 import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(format=app.config['LOG_FORMAT'], level=logging.ERROR,
+    logging.basicConfig(format=app.config['LOG_FORMAT'], level=logging.INFO,
                         datefmt='%Y-%m-%d %H:%M:%S')
     app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'], threaded=True)
